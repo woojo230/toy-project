4129a5315bb7171f0a65ccdc0588fa72134ce5c2
woojo230 <woojunghyck@naver.com>
2025-02-17 00:09:35 +0900

    useFetch hook 완료

--- react-final-project-skeleton/src/context/BookContext.jsx
+++ react-final-project-skeleton/src/context/BookContext.jsx
@@ -1,5 +1,11 @@
+import { useReducer } from 'react';
+import { bookReducer } from './BookReducer';
+
 export const BookProvider = () => {
-  return (
-    <></>
-  )
-}
+  const [state, dispatch] = useReducer(bookReducer, { books: [] });
+  cosnt[(filterParams, setFilterParams)] = useState({ genre: '', search: '' });
+  const queryString = new URLSearchParams(filterParams).toString();
+  const { data, loading, error } = useFetch(`/books?${queryString}`);
+
+  return <></>;
+};
